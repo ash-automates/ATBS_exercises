3f07367e4cdcbe3c06ab128d6db218ee662c173c
shachem <sekki.hachem@outlook.com>
2022-06-04 21:01:54 +0100

    Add initial setup: shebang line for windows, modules and TODOs

---
+++ chapter_07-Extract_Phone_and_Email/phoneAndEmail.py
@@ -0,0 +1,12 @@
+#! python3
+# phoneAndEmail.py - Finds phone numbers & email addresses on the clipboard
+
+import pyperclip, re
+
+# TODO: Create phone regex.
+
+# TODO: Create email regex.
+
+# TODO: Find matches in clipboard text.
+
+# TODO: Copy results to the clipboard.
