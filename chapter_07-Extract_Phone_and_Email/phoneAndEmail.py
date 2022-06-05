76f4efbf0b406f54e60185be15c6c6b145fb7173
shachem <sekki.hachem@outlook.com>
2022-06-05 11:05:08 +0100

    Create a regular expression to match US phone numbers

--- chapter_07-Extract_Phone_and_Email/phoneAndEmail.py
+++ chapter_07-Extract_Phone_and_Email/phoneAndEmail.py
@@ -3,8 +3,16 @@
 
 import pyperclip, re
 
-# TODO: Create phone regex.
-
+phoneRegex = re.compile(
+    r"""(
+    (\d{3}|\(\d{3}\))?  # Area Code
+    (\s|-|\.)?          # Separator
+    (\d{3})             # First 3 digits
+    (\s|-|\.)           # Separator
+    ({\d{4}})           # Last 4 digits
+    )""",
+    re.VERBOSE,
+)
 # TODO: Create email regex.
 
 # TODO: Find matches in clipboard text.
