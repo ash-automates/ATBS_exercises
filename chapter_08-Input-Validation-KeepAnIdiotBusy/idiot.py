b47d33f8519971e3fe94c614f3b32ca93eb475a4
shachem <sekki.hachem@outlook.com>
2022-06-07 22:38:21 +0100

    Add prank infinite loop to only break when input is 'no'

--- chapter_08-Input-Validation-KeepAnIdiotBusy/idiot.py
+++ chapter_08-Input-Validation-KeepAnIdiotBusy/idiot.py
@@ -1,1 +1,8 @@
 import pyinputplus as pyip
+
+prompt = "Want to know how to keep an idiot busy all day?\n"
+while True:
+    res = pyip.inputYesNo(prompt)
+    if res == "no":
+        print("Ok, have a nice day!")
+        break
