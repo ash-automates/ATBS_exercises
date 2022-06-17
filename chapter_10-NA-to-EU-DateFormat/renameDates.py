c45caac7991c89b7100977c39afb03cb498050de
shachem <sekki.hachem@outlook.com>
2022-06-17 16:58:04 +0100

    Assign each part of the filename to a different variable

--- chapter_10-NA-to-EU-DateFormat/renameDates.py
+++ chapter_10-NA-to-EU-DateFormat/renameDates.py
@@ -5,27 +5,30 @@
 import shutil, os, re
 
 # Create a regex that matches files with the American date format.
-dateRegex = re.compile(
+datePattern = re.compile(
     r"""
-    ^(.*?)              # all text before the date
-    ((0|1)?\d)-         # one or two digits for the month
-    ((0|1|2|3)?\d)-     # one or two digits for the day
-    ((19|20)\d\d)       # four digits for the year
-    (.*?)$              # all text after the date
+    ^(.*?)          # all text before the date
+    ((0|1)?\d)-     # one or two digits for the month
+    ((0|1|2|3)?\d)- # one or two digits for the day
+    ((19|20)\d\d)   # four digits for the year
+    (.*?)$          # all text after the date
     """,
     re.VERBOSE,
 )
 
 # Loop over the files in the working directory.
 for filename in os.listdir(os.getcwd()):
-    match = dateRegex.search(filename)
-
+    match = datePattern.search(filename)
     # Skip files without a date.
     if match == None:
         continue
 
-    # TODO: Get the different parts of the filename.
-
+    # Get the different parts of the filename.
+    beforePart = mo.group(1)
+    monthPart = mo.group(2)
+    dayPart = mo.group(4)
+    yearPart = mo.group(6)
+    afterPart = mo.group(8)
     # TODO: Form the European-style filename.
 
     # TODO: Get the full, absolute file paths.
