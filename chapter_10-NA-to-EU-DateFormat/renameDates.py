19a30b02c18696acf43f50f1e077c249fc7e4122
shachem <sekki.hachem@outlook.com>
2022-06-17 09:51:43 +0100

    Make skeleton script with comments and basic setup for Chapter 10 - Project A

---
+++ chapter_10-NA-to-EU-DateFormat/renameDates.py
@@ -0,0 +1,19 @@
+#! python3
+# renameDates.py - Renames filenames with American MM-DD-YYYY date format
+# to European DD-MM-YYYY.
+
+import shutil, os, re
+
+# TODO: Create a regex that matches files with the American date format.
+
+# TODO: Loop over the files in the working directory.
+
+# TODO: Skip files without a date.
+
+# TODO: Get the different parts of the filename.
+
+# TODO: Form the European-style filename.
+
+# TODO: Get the full, absolute file paths.
+
+# TODO: Rename the files.
