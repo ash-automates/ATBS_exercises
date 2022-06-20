a17a5aa710fec7e4d93a0fa929f41921eb74807d
shachem <sekki.hachem@outlook.com>
2022-06-20 16:02:18 +0100

    Figure out the new back up file number based on previous backups

--- chapter_10-Backup-To-Zip/backupToZip.py
+++ chapter_10-Backup-To-Zip/backupToZip.py
@@ -4,8 +4,18 @@
 
 import zipfile, os
 
-# TODO: Figure out the new file number based on the existing files
 
-# TODO: Create the ZIP file.
+def backupToZip(folder):
+    folder_path = os.path.abspath(folder)  # Make sure folder path is absolute
 
-# TODO: Walk the entire folder tree and compress the files in each folder.
+    # Figure out the new back up file number based on previous backups
+    count = 1
+    while True:
+        name_to_check = os.path.basename(folder) + "_" + str(count) + ".zip"
+        if not os.path.exists(name_to_check):
+            break
+        count += 1
+
+    # TODO: Create the ZIP file.
+
+    # TODO: Walk the entire folder tree and compress the files in each folder.
