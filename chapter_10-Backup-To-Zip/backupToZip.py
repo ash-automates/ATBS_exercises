1f192bcb010ebd5154d79bf8b5e4c95f780647d0
shachem <sekki.hachem@outlook.com>
2022-06-20 15:45:03 +0100

    Make skeleton script with comments and basic setup for Chapter 10 - Project B

---
+++ chapter_10-Backup-To-Zip/backupToZip.py
@@ -0,0 +1,11 @@
+#! python3
+# backupToZip.py - Copies an entire folder and its contents into
+# a ZIP file whose filename increments.
+
+import zipfile, os
+
+# TODO: Figure out the new file number based on the existing files
+
+# TODO: Create the ZIP file.
+
+# TODO: Walk the entire folder tree and compress the files in each folder.
