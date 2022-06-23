8463a3351c57606a5d91e807c1c1116c1f6270ea
shachem <sekki.hachem@outlook.com>
2022-06-23 13:04:48 +0100

    Make skeleton script with comments and basic setup for Chapter 12 - Project A

---
+++ chapter_11-mapit/mapIt.py
@@ -0,0 +1,9 @@
+#! python3
+# mapIt.py - Launches a map in the browser using an address from the
+# command line or clipboard.
+
+# TODO: Read the command line arguments from sys.argv
+
+# TODO: Get address from clipboard or command line argument
+
+# TODO: Open the web browser to the Google Maps page for the address
