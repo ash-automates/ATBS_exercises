13302edc1e56655178fdcce62d5fd71846de1b12
shachem <sekki.hachem@outlook.com>
2022-06-23 14:48:12 +0100

    Get address from clipboard or command line argument

--- chapter_11-mapit/mapIt.py
+++ chapter_11-mapit/mapIt.py
@@ -2,8 +2,12 @@
 # mapIt.py - Launches a map in the browser using an address from the
 # command line or clipboard.
 
-# TODO: Read the command line arguments from sys.argv
+import sys, webbrowser, pyperclip
 
-# TODO: Get address from clipboard or command line argument
+# Get address from clipboard or command line argument
+if len(sys.argv) > 1:
+    address = " ".join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
 
 # TODO: Open the web browser to the Google Maps page for the address
