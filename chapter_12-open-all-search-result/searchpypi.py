3149a05f9365c0a3dfc164640075ad7bca55b2ac
shachem <sekki.hachem@outlook.com>
2022-06-25 22:04:15 +0100

    Make skeleton script with comments and basic setup for Chapter 12 - Project B

---
+++ chapter_12-open-all-search-result/searchpypi.py
@@ -0,0 +1,10 @@
+#! python3
+# searchpypi.py - Opens several search results.
+
+import bs4, requests, webbrowser, sys
+
+# TODO: Fetch results webpage for queries given by sys.argv with requests module
+
+# TODO: Retrieve top search result links with bs4 module
+
+# TODO: Open a browser tab for each result with webbrowser module
