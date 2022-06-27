8053ed5ed0cc668139cd557152aefc9e983bf1fb
shachem <sekki.hachem@outlook.com>
2022-06-27 23:51:49 +0100

    Retrieve top search result links with bs4 module

--- chapter_12-open-all-search-result/searchpypi.py
+++ chapter_12-open-all-search-result/searchpypi.py
@@ -5,13 +5,11 @@ import bs4, requests, webbrowser, sys
 
 # Fetch results webpage for queries given by sys.argv with requests.get
 print("Searching.....")
-res = requests.get(
-    "https://google.com/search?q="
-    + "https://pypi.org/search/?q="
-    + " ".join(sys.argv[1:])
-)
+res = requests.get("https://pypi.org/search/?q=" + " ".join(sys.argv[1:]))
 res.raise_for_status()
 
-# TODO: Retrieve top search result links with bs4 module
+# Retrieve top search result links with bs4 module
+soup = bs4.BeautifulSoup(res.text, "lxml")
+links = soup.select(".package-snippet")
 
 # TODO: Open a browser tab for each result with webbrowser module
