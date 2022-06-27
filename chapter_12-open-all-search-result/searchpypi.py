f9a5a939f7981c57295082b5ddcbac1f3cbe0af2
shachem <sekki.hachem@outlook.com>
2022-06-27 23:33:51 +0100

    Fetch results webpage for queries given by sys.argv with requests.get

--- chapter_12-open-all-search-result/searchpypi.py
+++ chapter_12-open-all-search-result/searchpypi.py
@@ -3,7 +3,14 @@
 
 import bs4, requests, webbrowser, sys
 
-# TODO: Fetch results webpage for queries given by sys.argv with requests module
+# Fetch results webpage for queries given by sys.argv with requests.get
+print("Searching.....")
+res = requests.get(
+    "https://google.com/search?q="
+    + "https://pypi.org/search/?q="
+    + " ".join(sys.argv[1:])
+)
+res.raise_for_status()
 
 # TODO: Retrieve top search result links with bs4 module
 
