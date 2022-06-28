21891a059086fd8bf10b779e71c43525cd5cfd0a
shachem <sekki.hachem@outlook.com>
2022-06-28 23:48:07 +0100

    Make skeleton script with comments and basic setup for Chapter 12 - Project C

---
+++ chapter_12-download-Xkcd-comics/downloadXkcd.py
@@ -0,0 +1,16 @@
+#! python3
+# downloadXkcd.py - Downloads every single XKCD comic.
+
+import requests, os, bs4
+
+# TODO: Create folder to store the comics & store the front page URL in a var
+
+# TODO: Download the page
+
+# TODO: Find the URL of the comic image
+
+# TODO: Download the image
+
+# TODO: Save the image to the folder
+
+# TODO: Get the Prev button's url
