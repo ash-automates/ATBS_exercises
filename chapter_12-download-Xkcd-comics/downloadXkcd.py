559bf4cfedd44d74157e00d8df841b520b6351c7
shachem <sekki.hachem@outlook.com>
2022-06-29 04:51:16 +0100

    Download the HTML page within each iteration

--- chapter_12-download-Xkcd-comics/downloadXkcd.py
+++ chapter_12-download-Xkcd-comics/downloadXkcd.py
@@ -7,12 +7,18 @@ import requests, os, bs4
 url = "https://xkcd.com"
 os.makedirs("xkcdComics", exist_ok=True)
 
-# TODO: Download the page
+while not url.endswith("#"):
+    # Download the page
+    print(f"Downloading page '{url}'")
+    res = requests.get(url)
+    res.raise_for_status()
 
-# TODO: Find the URL of the comic image
+    # TODO: Find the URL of the comic image
 
-# TODO: Download the image
+    # TODO: Download the image
 
-# TODO: Save the image to the folder
+    # TODO: Save the image to the folder
 
-# TODO: Get the Prev button's url
+    # TODO: Get the Prev button's url
+
+print("Done!")
