aab9ebfa81cc23b23e4b8cbf485aeb329d733cd0
shachem <sekki.hachem@outlook.com>
2022-07-03 15:07:47 +0100

    For each row, check whether the value in column A is Celery, Garlic, or Lemon

--- chapter_13-Update-ProduceSpreadSheet/updateProduce.py
+++ chapter_13-Update-ProduceSpreadSheet/updateProduce.py
@@ -9,8 +9,13 @@ PRICE_UPDATES = {"Garlic": 3.07, "Celery": 1.19, "Lemon": 1.27}
 # Open the spreadsheet file & load the produce sheet onto the program
 wb = openpyxl.load_workbook("chapter_13-Update-ProduceSpreadSheet/produceSales.xlsx")
 sheet = wb["Sheet"]
-# TODO: For each row, check whether the value in column A is Celery, Garlic, or Lemon
 
-# TODO: If it is, update the price in column B
+# For each row, check whether the value in column A is Celery, Garlic, or Lemon
+for rowNum in range(2, sheet.max_row):
+    produce_name = sheet.cell(row=rowNum, column=1).value
+    if produce_name in PRICE_UPDATES:
+        pass
 
-# TODO: Save spreadsheet to new file so we don't lose original data
+        # TODO: If it is, update the price in column B
+
+        # TODO: Save spreadsheet to new file so we don't lose original data
