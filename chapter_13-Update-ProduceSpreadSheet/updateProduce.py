6524956e04afd08d48a1254f7993fbb7a609be17
shachem <sekki.hachem@outlook.com>
2022-07-03 15:01:20 +0100

    Open the spreadsheet file & load the produce sheet onto the program

--- chapter_13-Update-ProduceSpreadSheet/updateProduce.py
+++ chapter_13-Update-ProduceSpreadSheet/updateProduce.py
@@ -6,8 +6,9 @@ import openpyxl
 # The produce types and their updated prices
 PRICE_UPDATES = {"Garlic": 3.07, "Celery": 1.19, "Lemon": 1.27}
 
-# TODO: Open the spreadsheet file
-
+# Open the spreadsheet file & load the produce sheet onto the program
+wb = openpyxl.load_workbook("chapter_13-Update-ProduceSpreadSheet/produceSales.xlsx")
+sheet = wb["Sheet"]
 # TODO: For each row, check whether the value in column A is Celery, Garlic, or Lemon
 
 # TODO: If it is, update the price in column B
