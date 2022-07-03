b08f23596953f0ddd25b037399c563f20311778b
shachem <sekki.hachem@outlook.com>
2022-07-03 14:52:36 +0100

    Make skeleton script with comments and basic setup for Chapter 13 - Project B

---
+++ chapter_13-Update-ProduceSpreadSheet/updateProduce.py
@@ -0,0 +1,15 @@
+#! python3
+#  updateProduce.py - Corrects costs in produce sales spreadsheet
+
+import openpyxl
+
+# The produce types and their updated prices
+PRICE_UPDATES = {"Garlic": 3.07, "Celery": 1.19, "Lemon": 1.27}
+
+# TODO: Open the spreadsheet file
+
+# TODO: For each row, check whether the value in column A is Celery, Garlic, or Lemon
+
+# TODO: If it is, update the price in column B
+
+# TODO: Save spreadsheet to new file so we don't lose original data
