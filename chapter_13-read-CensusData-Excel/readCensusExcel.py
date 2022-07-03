02db6c8cb57c19d0c9abbfcf43ee865cb6fd8154
shachem <sekki.hachem@outlook.com>
2022-07-03 12:44:09 +0100

    Make skeleton script with comments and basic setup for Chapter 13 - Project A

---
+++ chapter_13-read-CensusData-Excel/readCensusExcel.py
@@ -0,0 +1,11 @@
+#! python3
+#  readCensusExcel.py - Tabulates population and number of census tracts for
+#  each county.
+
+import openpyxl, pprint
+
+# TODO: Open and read the cells of an Excel document with the openpyxl module
+
+# TODO: Calculate all the tract and population data and store it in a data structure
+
+# TODO: Write the data to a file with .py extension using the pprint module
