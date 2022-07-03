116a6146c0d62a6ffecbf147f84d2e1c7dcfe4e3
shachem <sekki.hachem@outlook.com>
2022-07-03 13:32:33 +0100

    Calculate all the tract and population data and store it in a data structure

--- chapter_13-read-CensusData-Excel/readCensusExcel.py
+++ chapter_13-read-CensusData-Excel/readCensusExcel.py
@@ -6,11 +6,27 @@ import openpyxl, pprint
 
 output_data = {}
 
-# Open and read the cells of an Excel document with the openpyxl module
+# Open and read the cells of the Excel document with the openpyxl module
 print("Opening workbook....")
 spreadsheet = openpyxl.load_workbook("censuspopdata.xlsx")
 main_sheet = spreadsheet["Population by Census Tract"]
 
-# TODO: Calculate all the tract and population data and store it in a data structure
+# Calculate all the tract and population data and store it in a data structure
+for row in range(2, main_sheet.max_row + 1):
+    # Each row in the spreadsheet has data for one census tract
+    state = main_sheet["B" + str(row)].value
+    county = main_sheet["C" + str(row)].value
+    population = main_sheet["D" + str(row)].value
+
+    # Make sure the key for this state exists
+    output_data.setdefault(state, {})
+    # Make sure the key for this county in this state exists
+    output_data[state].setdefault(county, {"pupulation": 0, "tracts": 0})
+
+    # Each row represents one census tract, so increment by one
+    output_data[state][county]["tracts"] += 1
+    # Increase the county population by the population in this census tract
+    output_data[state][county]["population"] += int(population)
+
 
 # TODO: Write the data to a file with .py extension using the pprint module
