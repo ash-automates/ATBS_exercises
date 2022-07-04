406b3d27e0f0f32533f35c54000d6014de05162e
shachem <sekki.hachem@outlook.com>
2022-07-04 17:08:51 +0100

    Loop through every file in the current working directory & skip non-CSV files

--- chapter_16-Remove-All-CSV-Headers/removeCsvHeader.py
+++ chapter_16-Remove-All-CSV-Headers/removeCsvHeader.py
@@ -7,8 +7,11 @@ import csv, os
 # Create a folder for the headless CSV files to be stored in
 os.makedirs("headless CSVs", exist_ok=True)
 
-# TODO: Loop through every file in the current working directory & skip non-CSV files
+# Loop through every file in the current working directory & skip non-CSV files
+for file_name in os.listdir("."):
+    if not file_name.endswith(".csv"):
+        continue
 
-# TODO: Read each CSV file into memory while skipping its first row
+    # TODO: Read each CSV file into memory while skipping its first row
 
-# TODO: Write out the headless CSV file & save it into the folder
+    # TODO: Write out the headless CSV file & save it into the folder
