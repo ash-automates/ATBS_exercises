86a0cad8f2b6fa5d8cda3d68856e473239f199bd
shachem <sekki.hachem@outlook.com>
2022-07-04 17:16:05 +0100

    Read each CSV file into memory while skipping its first row

--- chapter_16-Remove-All-CSV-Headers/removeCsvHeader.py
+++ chapter_16-Remove-All-CSV-Headers/removeCsvHeader.py
@@ -5,13 +5,21 @@
 import csv, os
 
 # Create a folder for the headless CSV files to be stored in
-os.makedirs("headless CSVs", exist_ok=True)
+os.makedirs("headless_csv_files", exist_ok=True)
 
 # Loop through every file in the current working directory & skip non-CSV files
 for file_name in os.listdir("."):
     if not file_name.endswith(".csv"):
         continue
 
-    # TODO: Read each CSV file into memory while skipping its first row
+    # Read each CSV file into memory while skipping its first row
+    copied_rows = []
+    csv_file_object = open(file_name)
+    reader_object = csv.reader(csv_file_object)
+    for row in reader_object:
+        if reader_object.line_num == 1:
+            continue
+        copied_rows.append(row)
+    csv_file_object.close()
 
     # TODO: Write out the headless CSV file & save it into the folder
