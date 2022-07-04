de589fdf0223ba04174c11fb43ec4c802b113049
shachem <sekki.hachem@outlook.com>
2022-07-04 17:05:08 +0100

    Make skeleton script with comments and basic setup for Chapter 16 - Project A

---
+++ chapter_16-Remove-All-CSV-Headers/removeCsvHeader.py
@@ -0,0 +1,13 @@
+#! python3
+#  removeCsvHeader.py - Removes the header from all CSV files in the current
+#  working directory.
+
+import csv, os
+
+# TODO: Create a folder for the headless CSV files to be stored in
+
+# TODO: Loop through every file in the current working directory & skip non-CSV files
+
+# TODO: Read each CSV file into memory while skipping its first row
+
+# TODO: Write out the headless CSV file & save it into the folder
