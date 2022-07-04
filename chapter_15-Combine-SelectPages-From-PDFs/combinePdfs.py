56c0fe10e0c6107808ac1b4480390a978799a5b0
shachem <sekki.hachem@outlook.com>
2022-07-04 14:43:19 +0100

    Make skeleton script with comments and basic setup for Chapter 15 - Project A

---
+++ chapter_15-Combine-SelectPages-From-PDFs/combinePdfs.py
@@ -0,0 +1,17 @@
+#! python3
+#  combinePdfs.py - Combines all the PDFs in the current working directory into
+#  into a single PDF.
+
+import PyPDF2, os
+
+# TODO: Get all the PDF filenames
+
+# TODO: Sort the filenames so the PDFs are added in order
+
+# TODO: Create a PdfFileWriter object for the output PDF
+
+# TODO: Loop through all the PDF files
+
+# TODO: Loop through all the pages (except the first) and add them
+
+# TODO: Save the resulting PDF to a file
