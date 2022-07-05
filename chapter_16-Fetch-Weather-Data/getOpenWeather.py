836700cf63d7006f37bb2a6e646d74203f0f7006
shachem <sekki.hachem@outlook.com>
2022-07-05 16:49:32 +0100

    Download the JSON data from OpenWeatherMap.org's API

--- chapter_16-Fetch-Weather-Data/getOpenWeather.py
+++ chapter_16-Fetch-Weather-Data/getOpenWeather.py
@@ -4,7 +4,7 @@
 import sys, pathlib, os, json, requests
 
 # get an API key from openweathermap.org and save it to "OpenWeather-API.txt" in home folder
-API_KEY = open(os.path.join(pathlib.Path.home(), "OpenWeather-API.txt")).read()
+KEY = open(os.path.join(pathlib.Path.home(), "OpenWeather-API.txt")).read()
 
 # Compute location from command line arguments & store it in a variable
 if len(sys.argv) < 2:
@@ -12,6 +12,9 @@ if len(sys.argv) < 2:
     sys.exit()
 location = " ".join(sys.argv[1:])
 
-# TODO: Download the JSON data from OpenWeatherMap.org's API
+# Download the JSON data from OpenWeatherMap.org's API
+url = f"https://api.openweathermap.org/data/2.5/forecast/daily?q={location}&cnt=3&APPID={KEY}"
+res = requests.get(url)
+res.raise_for_status()
 
 # TODO: Load JSON data into a Python variable
