888b3a31a9373d25024e990be9e2a8e10ba2b809
shachem <sekki.hachem@outlook.com>
2022-07-07 19:40:18 +0100

    Make skeleton script with comments and basic setup for Chapter 17 - Project B

---
+++ chapter_17-Multithread-XKCD-Download/threadedDownloadXkcd.py
@@ -0,0 +1,24 @@
+#! python3
+#  threadedDownloadXkcd.py - Downloads XKCD comics using multiple threads
+
+import requests, bs4, os, threading
+
+# TODO: Create folder to store the comics & store the front page URL in a var
+
+
+# TODO: Create function with core program to use as target for multithreading
+
+# TODO: Download the page
+
+# TODO: Find the URL of the comic image
+
+# TODO: Download the image
+
+# TODO: Save the image to the folder
+
+# TODO: Get the Prev button's url
+
+
+# TODO: Create and start the Thread objects
+
+# TODO: Wait for all threads to end.
